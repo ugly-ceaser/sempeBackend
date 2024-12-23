b3e2045c60bff25d58044fd3cf58c9fe0a0c9ab0
ugly-ceaser <martins.paraclet@yahoo.com>
2024-12-23 11:54:50 +0100

    render 5

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -28,7 +28,7 @@ adminRoute.route("/users").get(validateToken, adminCheck, getAllUsers);
 adminRoute.route("/user/:userId").get(validateToken, adminCheck, getUser);
 adminRoute.route("/user/:userId/:action").post(validateToken, adminCheck, adminActivateOrDeactivateUser);
 adminRoute.route("/user/:userId").put(validateToken, adminCheck,galleriestUpload.single("gallery_img"), uploadGalleryImg);
-adminRoute.route('users/:userId').post(validateToken, adminCheck,adminVerifyUser)
+adminRoute.route('/user/:userId').post(validateToken, adminCheck,adminVerifyUser)
 
 adminRoute.route("/user/:userId").delete(validateToken, adminCheck, deleteUser);
 
